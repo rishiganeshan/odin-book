51fbe00fe3e4cf862187f7f2cb2473fb03e5920d
Rishi Ganeshan <rishi9aneshan@gmail.com>
2025-09-09 14:05:29 +0100

    Refactor and create function to show latest book through DOM manipulation

--- script.js
+++ script.js
@@ -17,26 +17,12 @@ function addBookToLibrary(title, author, numberOfPages, haveRead) {
 
 const showButton = document.getElementById("showDialog");
 const favDialog = document.getElementById("favDialog");
-const selectEl = favDialog.querySelector("select");
 const confirmBtn = favDialog.querySelector("button");
 const form = document.querySelector("form");
 
 showButton.addEventListener("click", () => {
     favDialog.showModal();
 });
-
-favDialog.addEventListener("close", (e) => {
-    outputBox.value =
-        favDialog.returnValue === "default"
-            ? "No return value."
-            : `ReturnValue: ${favDialog.returnValue}.`; 
-});
-
-// confirmBtn.addEventListener("click", (event) => {
-//     event.preventDefault(); 
-//     favDialog.close(selectEl.value);
-// });
-
 
 
 
@@ -54,67 +40,35 @@ form.addEventListener("submit", (e) => {
 
     addBookToLibrary(formData.get("title"), 
         formData.get("author"),
-        formData.get("numberOfPages"),
-        formData.get("haveRead")
+        formData.get("pages"),
+        formData.get("read")
     )
-    let book = myLibrary.at(-1)
-    const div = document.createElement("div");
 
-    for (let prop in book) {
-        const property = document.createElement("div");
-        property.textContent = prop;
-        div.appendChild(property)
+    showLatestBook();
+    
+    favDialog.close()
 
-        const propval = document.createElement("div");
-        console.log(book[prop])
-        propval.textContent = book[prop];
-        div.appendChild(propval);
-
-
-        div.appendChild(document.createElement("br"))
-
-
-
-    }
-    div.appendChild(document.createElement("br"))
-    div.appendChild(document.createElement("br"))
-
-    // console.log(book)
-    body.appendChild(div)
 
 
 
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-addBookToLibrary("The Hobbit", "JRR Toll", 1234, true)
-addBookToLibrary("Harry Potter", "JK Rowling", 2834, true)
-addBookToLibrary("Moneyball", "Michael Lewis", 1234, true)
-
-console.log(myLibrary)
-
 const body = document.querySelector("body")
 
-console.log(myLibrary[1].title)
 
 
-for (let i=0; i < myLibrary.length; i++) {
-    let book = myLibrary[i]
+addBookToLibrary("The Hobbit", "JRR Toll", 1234, 'yes')
+showLatestBook()
+
+addBookToLibrary("Harry Potter", "JK Rowling", 2834, 'yes')
+showLatestBook()
+
+addBookToLibrary("Moneyball", "Michael Lewis", 1234, 'yes')
+showLatestBook()
+
+
+function showLatestBook() {
+    let book = myLibrary.at(-1)
+    console.log(book)
     const div = document.createElement("div");
 
     for (let prop in book) {
@@ -138,4 +92,5 @@ for (let i=0; i < myLibrary.length; i++) {
 
     // console.log(book)
     body.appendChild(div)
+
 }
