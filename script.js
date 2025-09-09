77141ee712ad357f97c9bd71966f8ae0aa6415f8
Rishi Ganeshan <rishi9aneshan@gmail.com>
2025-09-09 13:55:44 +0100

    Create responsive modal form to add new book

--- script.js
+++ script.js
@@ -6,15 +6,99 @@ function Book(title, author, numberOfPages, haveRead) {
     this.author = author;
     this.numberOfPages = numberOfPages;
     this.haveRead = haveRead;
-    // the constructor...
 
 }
 
 function addBookToLibrary(title, author, numberOfPages, haveRead) {
-    // take params, create a book then store it in the array
     let book = new Book(title, author, numberOfPages, haveRead);
     myLibrary.push(book);
 }
+
+
+const showButton = document.getElementById("showDialog");
+const favDialog = document.getElementById("favDialog");
+const selectEl = favDialog.querySelector("select");
+const confirmBtn = favDialog.querySelector("button");
+const form = document.querySelector("form");
+
+showButton.addEventListener("click", () => {
+    favDialog.showModal();
+});
+
+favDialog.addEventListener("close", (e) => {
+    outputBox.value =
+        favDialog.returnValue === "default"
+            ? "No return value."
+            : `ReturnValue: ${favDialog.returnValue}.`; 
+});
+
+// confirmBtn.addEventListener("click", (event) => {
+//     event.preventDefault(); 
+//     favDialog.close(selectEl.value);
+// });
+
+
+
+
+
+form.addEventListener("submit", (e) => {
+    // ...or iterate through the name-value pairs
+    e.preventDefault();
+
+    var formData = new FormData(form);
+
+    console.log(formData.get("title"))
+    for (var pair of formData.entries()) {
+        console.log(pair[0] + ": " + pair[1]);
+    }
+
+    addBookToLibrary(formData.get("title"), 
+        formData.get("author"),
+        formData.get("numberOfPages"),
+        formData.get("haveRead")
+    )
+    let book = myLibrary.at(-1)
+    const div = document.createElement("div");
+
+    for (let prop in book) {
+        const property = document.createElement("div");
+        property.textContent = prop;
+        div.appendChild(property)
+
+        const propval = document.createElement("div");
+        console.log(book[prop])
+        propval.textContent = book[prop];
+        div.appendChild(propval);
+
+
+        div.appendChild(document.createElement("br"))
+
+
+
+    }
+    div.appendChild(document.createElement("br"))
+    div.appendChild(document.createElement("br"))
+
+    // console.log(book)
+    body.appendChild(div)
+
+
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
